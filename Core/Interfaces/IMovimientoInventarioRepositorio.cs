28221992681e6dbbac60394eff7d79efd892c4f6
unknown <melanyboxig@gmail.com>
2025-11-22 16:37:46 -0400

    interfaz

--- Core/Interfaces/IMovimientoInventarioRepositorio.cs
+++ Core/Interfaces/IMovimientoInventarioRepositorio.cs
@@ -9,6 +9,7 @@ namespace AlmacenLP.Core.Interfaces
         Task<MovimientoInventarioDTO> GetMovimientoInventario(string Codigo);
         Task<MovimientoInventarioDTO> PutMovimientoInventario(string Codigo, [FromBody] MovimientoInventarioDTO dto);
         Task<MovimientoInventarioDTO> PostMovimientoInventario([FromBody] MovimientoInventarioDTO dto);
+        Task<MovimientoInventarioDTO> PostDevuelto([FromBody] MovimientoInventarioDTO dto);
         Task<MovimientoInventarioDTO> DeleteMovimientoInventario(string Codigo);
         Task<List<MovimientoInventarioDistribucionDTO>> GetCargaDistribucion();
         Task<List<MovimientoInventarioDistribucionDTO>> GetCargaSucursal();
